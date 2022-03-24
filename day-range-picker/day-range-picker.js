ebee686df3de5dd3d47b78d40e0b24053e07e85c
Nazmul Alam <nazmul@monerbaricom.local>
2022-03-24 19:09:14 +0600

    packages exports updated

--- day-range-picker/day-range-picker.js
+++ day-range-picker/day-range-picker.js
@@ -245,7 +245,7 @@ const renderTable = function (opts, data, randId) {
   );
 };
 
-export let DayRangePicker = function (options) {
+export default let DayRangePicker = function (options) {
   let self = this;
   let opts = self.config(options);
   
